88e850c8660ef17e8dff6626a727af1fdd99cede
samve <sam.verner42@gmail.com>
2023-08-24 14:49:27 -0500

    build version

--- src/main/java/co/codingnomads/deployment/models/Hello.java
+++ src/main/java/co/codingnomads/deployment/models/Hello.java
@@ -1,10 +1,11 @@
 package co.codingnomads.deployment.models;
 
 
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "hello")
